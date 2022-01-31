f460054f8d3d36e4988aff3d04a3a91cddda696f
Arif Hussain Shigri <arif.shigri444@gmail.com>
2022-01-31 18:40:38 +0500

    fixed

--- IntercallMonitor/UDPListener.cs
+++ IntercallMonitor/UDPListener.cs
@@ -43,7 +43,7 @@ using IntercallMonitor.Models;
                             if (message !=null)
                                 await _hub.Clients.All.SendAsync("ReceiveMessage",  message.callType,message.isRoom,message.roomNumber);
                            
-                            await _hub.Clients.All.SendAsync("ReceiveMessage", "CALL",true,10);
+                           // await _hub.Clients.All.SendAsync("ReceiveMessage", "CALL",true,10);
 
                         }
                         catch (Exception ex) {  Console.WriteLine(ex.Message); }
