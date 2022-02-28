391bac77f223ae50bf0a26ef596380482c395647
arif-eurus <arif@eurustechnologies.com>
2022-03-01 02:11:58 +0500

    dev test

--- NurseCallMonitor/Startup.cs
+++ NurseCallMonitor/Startup.cs
@@ -43,7 +43,7 @@ namespace NurseCallMonitor
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            
             app.UseRouting();
 
             app.UseAuthorization();
