46dba6dcbbcdf69b3db71c77e722c2f7f6c149c8
arif-Eurus <arif@eurustechnologies.com>
2022-03-27 23:04:33 -0700

    git

--- NurseCallMonitor/Program.cs
+++ NurseCallMonitor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
@@ -13,7 +14,14 @@ namespace NurseCallMonitor
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = new WebHostBuilder()
+             .UseKestrel()
+             .UseContentRoot(Directory.GetCurrentDirectory())
+             .UseIISIntegration()
+             .UseStartup<Startup>()
+             .Build();
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
